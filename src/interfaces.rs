e6ceab60234442b9d37e4ae1d0c98bb58b8c7a24
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-09-13 18:04:36 +0200

    Added Recordable trait

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -9,6 +9,21 @@ pub trait Queryable {
     fn insert(&mut self, new_record: Vec<String>) -> Result<(), QueryError>;
 }
 
+pub trait Recordable {
+    fn get_record_as_collection(&self) -> Vec<String>; 
+    fn get_attr_index_from_name(&self, attr_name: &String) -> Result<usize, QueryError>;
+    fn get_attr_value(&self, attr_name: &String) -> Result<String, QueryError> {
+        let coll = self.get_record_as_collection();
+        let idx = self.get_attr_index_from_name(attr_name)?;
+        match coll.get(idx) {
+            Some(v) => Ok(v.clone()),
+            None => Err(QueryError),
+        }
+    }
+    fn delete_value(&mut self, attr_name: &String) -> Result<(), QueryError>;
+    fn update_value(&mut self, attr_name: &String, new_value: &String) -> Result<(), QueryError>;
+}
+
 #[derive(Debug, Clone)]
 pub enum Filter {
     Equal(String),
