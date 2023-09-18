049ebdbccf5b84da740cbac61537eeef0d290ebf
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-09-18 21:26:15 +0200

    Edited Loadable trait

--- src/traits.rs
+++ src/traits.rs
@@ -1,15 +1,23 @@
 use std::collections::HashMap;
 
-use crate::errors::{QueryError, FlushError, LoadingError};
+use crate::errors::{QueryError, CommitError, LoadingError};
 
 type ColumnName = String;
 type Value = Option<String>;
 pub trait Queryable<T: Recordable> {
-    fn bulk_load_data(&mut self, data: &impl Loadable) -> Result<(), LoadingError>;
+    fn bulk_load_data(&mut self, data: &Vec<Vec<Option<String>>>) -> Result<(), LoadingError> {
+        for r in data {
+            if let Err(_) = self.insert(InsertElement::PlainValues(r.clone())) {
+                return Err(LoadingError::InvalidRecord(String::new()));
+            }
+        }
+        Ok(())
+    }
     fn select(&self, attributes_names: Columns, conditions: &Option<Condition>) -> Result<Vec<Vec<Option<String>>>, QueryError>;
     fn delete(&mut self, conditions: &Option<Condition>) -> Result<(), QueryError>;
     fn update(&mut self, new_values: HashMap<ColumnName, Value>, conditions: &Option<Condition>) -> Result<(), QueryError>;
     fn insert(&mut self, new_record: InsertElement) -> Result<(), QueryError>;
+    fn get_records_as_collection(&self) -> Vec<Vec<Option<String>>>;
 }
 
 pub trait Recordable: Sized {
@@ -40,7 +48,16 @@ pub enum InsertElement {
 }
 
 
-pub trait Loadable: Sized {
-    fn bulk_data<'a>(&self) -> Result<Vec<Vec<Option<String>>>, LoadingError>;
-    fn commit(&self) -> Result<(), FlushError>;
+pub trait Loadable<T>: Sized
+where T: Recordable {
+    fn line_to_vec(line_string: &mut String, columns_amount: usize) -> Result<Vec<Option<String>>, LoadingError>;
+    fn collection_to_string(collection: Vec<Vec<Option<String>>>) -> String;
+    fn load_from_source(source_path: &str, source_type: SourceType) -> Result<Self, LoadingError>;
+    fn bulk_data(&self, columns_amount: usize) -> Result<Vec<Vec<Option<String>>>, LoadingError>;
+    fn commit(&mut self, query_subject: impl Queryable<T>) -> Result<(), CommitError>;
+}
+
+pub enum SourceType {
+    LocalFile,
+    Http,
 }
