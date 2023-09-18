fe99bd9f26ca41971c64bf9753c9b9eeae62cc78
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-09-18 21:27:18 +0200

    Created test module for file_parsing

---
+++ tests/file_parsing.rs
@@ -0,0 +1,25 @@
+use csql::entities::Table;
+
+use csql::errors::TableInitError;
+
+use csql::file_parsing::{Buffer, Source};
+
+use csql::traits::{Queryable, Loadable, Columns};
+
+#[test]
+fn apply_select() -> Result<(), TableInitError> {
+    let buf = Buffer::new(Source::FilePath(String::from("samples/example0.csv")));
+    let mut t = Table::new(None, &vec!["one", "two", "three"])?;
+    t.bulk_load_data(&buf.bulk_data(3).unwrap()).unwrap();
+    let expected: Vec<Vec<Option<String>>> = vec![
+        vec![Some(String::from("a")), Some(String::from("c"))],
+        vec![Some(String::from("1")), Some(String::from("3"))],
+        vec![Some(String::from("x")), Some(String::from("z"))],
+    ];
+    let query_res = t.select(
+        Columns::ColumnNames(vec![String::from("one"), String::from("three")]),
+        &None
+    ).unwrap();
+    assert_eq!(query_res, expected);
+    Ok(())
+}
