4153d3c49aa790f1124c87b6b2b9a2f24b6c35e7
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-10-03 12:16:25 +0200

    Print example for update

--- tests/commands.rs
+++ tests/commands.rs
@@ -4,21 +4,7 @@ use sqlparser::parser::Parser;
 #[test]
 fn feature() {
     let dialect = GenericDialect {};
-    let sql = "INSERT INTO t VALUES (1,2,3)";
+    let sql = "update t set a = 1, b = 2 where id > 3";
     let ast = Parser::parse_sql(&dialect, sql).unwrap();
-    let first = ast.first().unwrap();
-    if let Statement::Query(q) = first {
-        let s1 = &**q;
-        // println!("{:?}", s1.order_by);
-        let s2 = &**(&s1.body);
-        if let sqlparser::ast::SetExpr::Select(s) = s2 {
-            let select_body = &**s;
-            let from = select_body.from.first().unwrap();
-            let group_by = &select_body.group_by;
-            // println!("From : {:?}", from);
-            // println!("Group by : {:?}", group_by);
-            // println!("Order by : {:?}", order_by);
-        }
-    }
-    println!("{:#?}", first);
+    println!("{:#?}", ast);
 }
