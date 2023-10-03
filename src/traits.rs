dc2a4f51edabb7fabc322ad278125243caef7413
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-10-03 22:27:56 +0200

    Replaced Option<String> with alias : Value

--- src/traits.rs
+++ src/traits.rs
@@ -5,7 +5,7 @@ use crate::{errors::{CommitError, ExportError, LoadingError, QueryError, Seriali
 type ColumnName = String;
 type Value = Option<String>;
 pub trait Queryable<T: Recordable> {
-    fn bulk_load_data(&mut self, data: &Vec<Vec<Option<String>>>) -> Result<(), LoadingError> {
+    fn bulk_load_data(&mut self, data: &Vec<Vec<Value>>) -> Result<(), LoadingError> {
         for r in data {
             if let Err(_) = self.insert(InsertElement::PlainValues(r.clone())) {
                 return Err(LoadingError::InvalidRecord(String::new()));
@@ -17,7 +17,7 @@ pub trait Queryable<T: Recordable> {
         &self,
         attributes_names: Columns,
         conditions: &Option<Condition>,
-    ) -> Result<Vec<Vec<Option<String>>>, QueryError>;
+    ) -> Result<Vec<Vec<Value>>, QueryError>;
     fn delete(&mut self, conditions: &Option<Condition>) -> Result<(), QueryError>;
     fn update(
         &mut self,
@@ -25,14 +25,14 @@ pub trait Queryable<T: Recordable> {
         conditions: &Option<Condition>,
     ) -> Result<(), QueryError>;
     fn insert(&mut self, new_record: InsertElement) -> Result<(), QueryError>;
-    fn get_records_as_collection(&self) -> Vec<Vec<Option<String>>>;
+    fn get_records_as_collection(&self) -> Vec<Vec<Value>>;
 }
 
 pub trait Recordable: Sized {
-    fn get_record_as_collection(&self) -> Vec<Option<String>>;
+    fn get_record_as_collection(&self) -> Vec<Value>;
     fn get_attr_index_from_name(&self, attr_name: &String) -> Result<usize, QueryError>;
-    fn get_attr_value(&self, attr_name: &String) -> Result<Option<String>, QueryError>;
-    fn get_attr_values(&self, attr_names: &Vec<String>) -> Result<Vec<Option<String>>, QueryError>;
+    fn get_attr_value(&self, attr_name: &String) -> Result<Value, QueryError>;
+    fn get_attr_values(&self, attr_names: &Vec<String>) -> Result<Vec<Value>, QueryError>;
     fn update_values(&mut self, new_values: &HashMap<ColumnName, Value>) -> Result<(), QueryError>;
     fn satisfy_conditions(&self, cond: &Condition) -> Result<bool, QueryError>;
 }
@@ -62,11 +62,11 @@ where
     fn line_to_vec(
         line_string: &mut String,
         columns_amount: usize,
-    ) -> Result<Vec<Option<String>>, LoadingError>;
-    fn collection_to_string(collection: Vec<Vec<Option<String>>>) -> String;
+    ) -> Result<Vec<Value>, LoadingError>;
+    fn collection_to_string(collection: Vec<Vec<Value>>) -> String;
     fn load_from_source(source_path: &str, source_type: SourceType) -> Result<Self, LoadingError>;
-    fn bulk_data(&self, columns_amount: usize) -> Result<Vec<Vec<Option<String>>>, LoadingError>;
-    fn dump_data(&self, data: Vec<Vec<Option<String>>>) -> Result<(), ExportError>;
+    fn bulk_data(&self, columns_amount: usize) -> Result<Vec<Vec<Value>>, LoadingError>;
+    fn dump_data(&self, data: Vec<Vec<Value>>) -> Result<(), ExportError>;
     fn commit(&mut self, query_subject: &impl Queryable<T>) -> Result<(), CommitError> {
         self.dump_data(query_subject.get_records_as_collection())
             .map_err(|_| CommitError)
