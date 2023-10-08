5de1f39d36a1b1fdac67d45acded2eddd54c0ac3
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-10-08 15:14:44 +0200

    Print command select example

--- tests/commands.rs
+++ tests/commands.rs
@@ -1,10 +1,35 @@
+use csql::traits::Executable;
 use sqlparser::dialect::GenericDialect;
-use sqlparser::parser::Parser;
+use sqlparser::parser::{Parser, ParserError};
 
 #[test]
-fn feature() {
+fn parsing_select() -> Result<(), ParserError>{
     let dialect = GenericDialect {};
-    let sql = "update t set a = 1, b = 2 where id > 3";
-    let ast = Parser::parse_sql(&dialect, sql).unwrap();
-    println!("{:#?}", ast);
+    let sql = "
+    select * from user, post;
+    select name from user where id = 1000;
+    select name, email from user where id > 5 and id < 200";
+    let statements = Parser::parse_sql(&dialect, sql)?;
+    let commands: Result<Vec<_>, _> = statements.iter()
+    .map(|st| st.deserialize_as_command())
+    .collect();
+    if let Ok(comms) = &commands {
+        comms.iter().for_each(|c| println!("{:?}", c))
+    }
+    Ok(())
+}
+
+#[test]
+fn parsing_update() {
+    
+}
+
+#[test]
+fn parsing_insert() {
+    
+}
+
+#[test]
+fn parsing_delete() {
+    
 }
