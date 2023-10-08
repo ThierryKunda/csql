463475cc89b2024aec7af69f8b9ca20c541731bb
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-10-08 16:06:28 +0200

    Added delete command example

--- tests/commands.rs
+++ tests/commands.rs
@@ -62,6 +62,21 @@ fn parsing_insert() -> Result<(), ParserError> {
 }
 
 #[test]
-fn parsing_delete() {
-    
+fn parsing_delete() -> Result<(), ParserError> {
+    let dialect = GenericDialect {};
+    let sql = "
+    delete from user;
+    delete from user where name = 'john';
+    delete from user where age < 18 or name = 'john';
+    ";
+    let statements = Parser::parse_sql(&dialect, sql)?;
+    let commands: Result<Vec<_>, _> = statements.iter()
+    .map(|st| st.deserialize_as_command())
+    .collect();
+    if let Ok(comms) = &commands {
+        comms.iter().for_each(|c| println!("{:?}", c))
+    } else {
+        println!("{:?}", commands);
+    }
+    Ok(())
 }
